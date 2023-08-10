fbd1ff8905613b1fb5b5fa01bbaaf81b386545a2
HasanSabourSharif <hasan.sabour.sharif@gmail.com>
2023-08-10 22:59:05 +0330

    testidf

--- dockerfile
+++ dockerfile
@@ -6,7 +6,7 @@ COPY requirements.txt /app/
 COPY main.py /app/
 
 RUN pip install --no-cache-dir -r requirements.txt
-
+RUN python model.py
 EXPOSE 80
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 from fastapi import FastAPI
 app = FastAPI()
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.models import load_model
+import numpy as np
 
-@app.get("/ii/")
+
+model = load_model('mnist_model.h5')
+
+
+(x_test, y_test) = mnist.load_data()[1]
+x_test = x_test / 255.0
+
+
+num_samples = 5
+random_indices = np.random.choice(x_test.shape[0], num_samples, replace=False)
+images = x_test[random_indices]
+
+
+predictions = model.predict(images)
+
+
+@app.get("/testi/")
 def read_root():
-    return {"هدف": "سامانه پاسخگویی به سوالات شرعی"}
+    k=[]
+    for i in range(num_samples):
+        predicted_label = np.argmax(predictions[i])
+        actual_label = y_test[random_indices[i]]
+    k.append(f"Predicted: {predicted_label}, Actual: {actual_label}")
+
+    return k
+
+@app.get("/mtesti/")
+def read_root():
+    
+    return 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
