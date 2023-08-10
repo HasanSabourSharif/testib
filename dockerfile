aafbd54f1339c756ea38a927230fd1da9b0688ff
HasanSabourSharif <hasan.sabour.sharif@gmail.com>
2023-08-11 01:02:24 +0330

    Edit files

--- dockerfile
+++ dockerfile
@@ -7,7 +7,6 @@ COPY . /app/
 
 RUN pip install --no-cache-dir -r requirements.txt
 RUN gdown 1-8PGv5ElSSdgiHbtTK4J1O5eKMh5MySD -O pytorch_model.bin
-RUN mv ./pytorch_model.bin ./myModel/pytorch_model.bin
 
 EXPOSE 80
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from transformers import AutoModelForQuestionAnswering, AutoTokenizer
 from utils import AnswerPredictor
 
 # Load the model state dictionary
-model_state_dict = torch.load("./myModel/pytorch_model.bin", map_location=torch.device('cpu'))
+model_state_dict = torch.load("./pytorch_model.bin", map_location=torch.device('cpu'))
 
 # Create an instance of the model using its class definition
 model = AutoModelForQuestionAnswering.from_pretrained("HooshvareLab/bert-fa-base-uncased", state_dict=model_state_dict)
