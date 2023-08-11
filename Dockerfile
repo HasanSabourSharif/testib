63a0e370ab1022b081e2bbe4ca7adad029c0a447
smajy <sma.jahanmir@gmail.com>
2023-08-11 11:32:53 +0330

    edit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.11
+
+WORKDIR /app
+
+COPY requirements.txt /app/
+
+RUN python3 -m pip install -r ./requirements.txt
+
+RUN gdown 1-8PGv5ElSSdgiHbtTK4J1O5eKMh5MySD -O pytorch_model.bin
+RUN mv ./pytorch_model.bin ./myModel/pytorch_model.bin
+
+COPY . /app/
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import time
 import model
 
-app = FastAPI()
+app = FastAPI(
+    title='MLOps', debug=True,
+)
+
+@app.middleware("http")
+async def measure_execution_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    end_time = time.time()
+    execution_time = (end_time - start_time)
+    print(f"Endpoint '{request.method} {request.url.path}' execution time: {execution_time}s")
+    return response
+
 
 @app.post("/question")
 async def create_item(question:str):
